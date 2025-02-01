9bdc5d4a9f73db0b2ac2326e11f6600e5d97706f
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2025-02-01 04:37:45 +0000

    coding done

---
+++ 01.basic/01.variable.js
@@ -0,0 +1,15 @@
+const accountId = "123456789"
+let accountEmail = "Mia@google.com"
+var accountPass = "1234567890"
+accountCity = "New Delhi"
+
+
+
+console.table([accountId,accountEmail,accountPass,accountCity])
+
+
+accountEmail = "Maudaa@google.com"
+accountPass = "Maudaa"
+accountCity = "Delhi"
+
+console.table([accountId,accountEmail,accountPass,accountCity])
