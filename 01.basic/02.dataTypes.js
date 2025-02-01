a4a2c5aa676f68d4f317dc0c5ed5fffad345ce45
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2025-02-01 06:47:09 +0000

    coding done

---
+++ 01.basic/02.dataTypes.js
@@ -0,0 +1,38 @@
+// Primative Data Types //
+
+// Srting => "" //
+
+const name = "Miaa"
+console.log(typeof name, name);
+
+//Number => 2 to power 53 //
+
+let age = 28
+console.log(typeof age,age );
+
+// Boolean => true & false
+
+let isLoddedIn = false
+console.log(typeof isLoddedIn,isLoddedIn);
+
+// Bigint
+let account = "Bigint"
+console.log(account);
+
+// Undefind
+
+let akshay;
+console.log(akshay);
+
+// Null
+
+let pathak = null
+console.log(typeof pathak);
+
+//Symbol => unique
+
+let symbol = "symbol"
+console.log(symbol);
+
+
+
