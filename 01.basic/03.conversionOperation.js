59ed3dc9c540852e53c2b1b9e4e6ca746942d8f1
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2025-02-02 05:58:30 +0000

    coding done

---
+++ 01.basic/03.conversionOperation.js
@@ -0,0 +1,31 @@
+
+                            // Conversion Number
+let score = null
+// console.log(score);
+
+let valueNumber = Number(score)
+// console.log(valueNumber);
+// console.log(typeof valueNumber);
+
+
+                            // Conversion Boolean
+
+
+let BooleanValue = "Ak"
+let isLoddedIn = Boolean(BooleanValue)
+// console.log(isLoddedIn);
+
+
+                            // String Convert
+
+let someValue = 33
+let StringNumber = String(someValue)
+// console.log(StringNumber);
+// console.log(typeof StringNumber)
+
+
+
+let SrtingNumber = 34
+let SrtingValue = String(SrtingNumber)
+console.log(SrtingValue);
+console.log(typeof SrtingValue);
