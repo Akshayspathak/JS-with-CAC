2cc2e86ca314e2da3832a58971c89e6a14e24494
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2025-02-03 05:57:06 +0000

    coding done

--- 01.basic/03.conversionOperation.js
+++ 01.basic/03.conversionOperation.js
@@ -27,5 +27,37 @@ let StringNumber = String(someValue)
 
 let SrtingNumber = 34
 let SrtingValue = String(SrtingNumber)
-console.log(SrtingValue);
-console.log(typeof SrtingValue);
+// console.log(SrtingValue);
+// console.log(typeof SrtingValue);
+
+
+ //***********************************************************************************************************//
+
+
+
+
+
+// Operation //
+
+let value = 3
+console.log(-value);
+
+let str1 = "Akshay"
+let str2 = " Pathak"
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("1" + 2);
+console.log(1 + "2");
+
+
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
+let gameCounter = 100
+++gameCounter                 // Prefix madhe value same asta   2 value
+
+console.log(gameCounter);     // Postfix madhe value same naste 2 value
+      
