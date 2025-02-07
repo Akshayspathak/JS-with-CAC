d1172aa63f29bacaa32fbcdde8cb9f61c98fc3a3
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2025-02-07 09:54:40 +0000

    done

--- 01.basic/stack-Heap.js
+++ 01.basic/stack-Heap.js
@@ -2,13 +2,15 @@
 
 
 
+// Stack Primitive
+
 let work = "CV"
 let anotherWork = "JS"
 
 anotherWork = "VIshal"
 
-console.log(anotherWork);
-console.log(work);
+// console.log(anotherWork);
+// console.log(work);
 
 
 
@@ -21,6 +23,55 @@ let userTwo = userOne
 
 userOne.email = "Akshay@123.com"
 
-console.log(userTwo);
-console.log(userOne);
+// console.log(userTwo);
+// console.log(userOne);
 
+
+
+// Stack (Primitive) Data Type
+
+let user = "Aditya@google.com"
+let user2 = "Piya@google.com"
+
+user2 = "AAkash@yahoo.com"
+
+// console.log(user);
+// console.log(user2);
+
+
+// Heap (Nom-Primitive) 
+
+let userObj = {
+    email:"AKshay@google.com",
+    pass: "qwertyuiop" 
+}
+
+userObj.email = "Vaibhav@google.com"
+
+// console.log(userObj);
+
+
+
+
+
+let accountNumber = 20100174
+let accountNumber2 = 5555555555555555
+
+accountNumber2 = 888888888888888888
+
+// console.log(accountNumber);
+// console.log(accountNumber2);
+
+
+
+// Heap Non-Primitive
+
+
+let accountPassword = {
+    accountNumber: 12333654789,
+    accountPass: "asdfghjkl;poiuytreww"
+}
+
+accountPassword.accountPass = "Akshay"
+
+console.log(accountPassword);
