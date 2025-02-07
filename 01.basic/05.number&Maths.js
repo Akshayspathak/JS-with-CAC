58e0c3b5f4c0f3cdf74e52264be7e812666d5c48
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2025-02-07 12:22:29 +0000

    Math & Number

---
+++ 01.basic/05.number&Maths.js
@@ -0,0 +1,54 @@
+const account = new Number(200)
+// console.log(account);
+// console.log(account.toString());
+// console.log(typeof account);
+
+
+
+
+const password = new String("qwerttyui")
+// console.log(password);
+
+
+
+let score = 400
+// console.log(score);
+// console.log(typeof score);
+
+
+
+const value = new Number(3000.)
+// console.log(value);
+// console.log(value.toString());
+// console.log(typeof value);
+
+
+
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// -----------------------------------------------------------
+
+// console.log(Math);
+
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.3));
+// console.log((Math.floor(4.3)));
+// console.log(Math.ceil(4.3));
+// console.log(Math.min(9,6,5,2,0,1,4,7));
+// console.log(Math.max(8,6,2,4,3,13));
+
+
+
+
+
+// console.log(Math.random());                       ** single Number only formula **
+// console.log(Math.floor(Math.random()*10) + 1);
+
+
+const min = 100
+const max = 200
+console.log(Math.floor((Math.random() * (max - min + 1)) + min)) // Min or Max value
+
