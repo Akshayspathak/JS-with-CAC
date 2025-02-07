b58b1b2dc727b3592823aa106bc6789a578712fe
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2025-02-07 10:33:54 +0000

    done

---
+++ 01.basic/04.string.js
@@ -0,0 +1,19 @@
+const value = new String('akshays')
+
+console.log(value.length);
+console.log(value.toUpperCase());
+console.log(value.charAt(2));
+console.log(value.indexOf('s'));
+
+
+const otherName = "      Akshay      "
+console.log(otherName);
+console.log(otherName.trim());
+
+let accountName = "     Piyaa      "
+console.log(accountName);
+console.log(accountName.trim());
+
+
+
+
