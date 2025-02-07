c9f49ed6c69f8f67888aea662e6ecb0c68682f5f
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2025-02-07 09:39:14 +0000

    done

---
+++ 01.basic/stack-Heap.js
@@ -0,0 +1,26 @@
+// Stack (Primitive), Heap (Nom-Primitive)
+
+
+
+let work = "CV"
+let anotherWork = "JS"
+
+anotherWork = "VIshal"
+
+console.log(anotherWork);
+console.log(work);
+
+
+
+let userOne = {
+    email:"HC@HC.google.com",
+    Password: "1234556789"
+}
+
+let userTwo = userOne
+
+userOne.email = "Akshay@123.com"
+
+console.log(userTwo);
+console.log(userOne);
+
