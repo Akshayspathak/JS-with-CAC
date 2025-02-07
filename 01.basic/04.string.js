4cc8518ffe0faec5190252acda07e14513949ae8
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2025-02-07 10:38:43 +0000

    done

--- 01.basic/04.string.js
+++ 01.basic/04.string.js
@@ -1,5 +1,11 @@
-const value = new String('akshays')
+const petId = 2020202020202
+const password = "1212343456"
+console.log(`PetId is ${petId} & password is ${password}`);
 
+
+
+
+const value = new String('akshays')
 console.log(value.length);
 console.log(value.toUpperCase());
 console.log(value.charAt(2));
