d16751206d0ac00acddcbb7fd2c126c4b814c5a9
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2025-02-10 09:32:50 +0000

    done

---
+++ 02.basicPart/basic_array.js
@@ -0,0 +1,53 @@
+// const Heros = ["Adi","Piya","aakya"]
+// const dc_Heros = ["ankit", "Sanket"]
+
+// const all_Heros = Heros.concat(dc_Heros)
+// console.log(all_Heros);
+
+
+// const new_all_Heros = [...Heros, ...dc_Heros]
+// console.log(new_all_Heros);
+
+
+
+
+// spread operator
+
+// const cars = ["BMW","Audi","Maruti"]
+// const name = [" Q3","Q4","Q5"]
+// const carName =  console.log(...cars , ...name);
+
+// console.log(carName);
+// console.log(typeof carName);
+
+
+
+
+
+
+// const num = [1,2,3,4,5]
+// const num2 = [6,7,8,9,"Hitesh"]
+// const allNum = [...num, ...num2]
+// console.log(allNum);
+
+// const arr = [1,2,3,[4,5,6,[7,8,9,[11,22,33,44,55,["adi",["Piya"]]]]]]
+// const allMyArr = arr.flat(Infinity)
+// console.log(allMyArr);
+
+// console.log(Array.isArray("Akshay"));
+
+
+// console.log(Array.from("Akshay"));
+// console.log(Array.of("Akshay"));
+
+
+
+let score = 300
+let score2 = 400
+let score3 = 500
+console.log(Array.of(score,score2,score3));
+
+
+console.log(Array.from("piya"));
+console.log(Array.of("Piya"));
+
