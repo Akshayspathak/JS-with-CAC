c84c83962988addd23edca746e7b21f37ca4f53a
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2025-02-10 08:46:22 +0000

    array done

---
+++ 02.basicPart/array.js
@@ -0,0 +1,208 @@
+// Array
+
+// const myArr = [0,1,2,3,4,5,]
+// let myHeros = ["Aksh","Piya","Adi"]
+
+// const myArr2 = new Array(1,2,3,4)
+// console.log(myArr2);
+
+
+// console.log(myArr)
+// console.log(myHeros);
+// ;
+
+// myArr.push(6)
+// console.log(myArr);
+
+
+// const arr = [1,2,3,4,5]
+// console.log(arr);
+
+// const heros = ["Adi","Piya","Vai",true]
+// console.log(heros);
+
+
+// const newArr = new Array(5,4,7,9,3,5)
+// console.log(newArr);
+
+// *********************************************************************************************************************
+
+
+// Array Method
+
+
+// const method =  [1,2,3,4,5,6]
+ 
+// method.push(1)
+// console.log(method);
+
+// method.push(100)
+// console.log(method);
+
+// method.pop()
+// console.log(method);
+// method.pop()
+// console.log(method);
+// method.pop()
+// console.log(method);
+
+
+
+// method.unshift(100)
+// console.log(method);
+// method.unshift(500)
+// console.log(method);
+// method.unshift()
+
+// method.shift()
+// console.log(method);
+
+// method.shift()
+// console.log(method);
+
+// method.shift()
+// console.log(method);
+
+
+// const myArr = [1,2,3,4,5,6,7,8,9]
+// console.log(myArr);
+
+// const array = new Array(100,200,300,400,500,600)
+// console.log(array);
+
+
+// const myHeros = ["Manoj",true,false,"Adi"]
+// console.log(myHeros);
+//***************************************************************************************************************/
+
+
+// const array = [1,2,3,4,5,6,7,8]
+// console.log(array);
+
+// array.push(1000)
+// console.log(array);
+
+// array.pop()
+// console.log(array);
+
+
+// array.unshift("Hiteesh")
+// console.log(array);
+
+// array.shift()
+// console.log(array);
+
+// array.shift()
+// console.log(array);
+
+// array.pop()
+// console.log(array);
+
+// array.push("Adi")
+// console.log(array);
+
+// array.unshift(1000)
+// console.log(array);
+
+// array.pop()
+// console.log(array);
+// array.shift()
+// console.log(array);
+
+
+// const string = new String("adi")
+// console.log(string);
+
+// let account = 2292020202020
+// let password = "987654000656532"
+// let email = "hc@google.com"
+
+// console.log(`this is Account number is ${account}, and Password is ${password}, and Email ${email}`);
+
+// account = 1234567
+// console.log(`this is Account number is ${account}, and Password is ${password}, and Email ${email}`);
+// console.log(account);
+
+// password = "Hiteesh@123"
+// console.log(password);
+
+
+// password = "Adi@12345"
+// console.log(password);
+
+
+// account = "123123123"
+// console.log(account);
+
+
+
+// const array = [100,200,300,400,500,600,700,800,900]
+// console.log(array);
+
+// array.push(1000)
+// console.log(array);
+
+// array.pop()
+// console.log(array);
+
+// array.unshift("Hitesh")
+// console.log(array);
+
+// array.shift()
+// console.log(array);
+
+// let account = 21212121
+// let accountName = "Akshay pathak"
+// let accountPass = "Hc@Hc123"
+
+// console.log(`accountId is ${account}`);
+// console.log(`accountName is ${accountName}`);
+// console.log(`accountPass is ${accountPass}`);
+
+
+// account = 98989898
+// console.log(account);
+
+// let myArr = ["Hitesh", "Adi0"]
+// console.log(myArr.includes(0));
+// console.log(myArr.includes("Adi0"));
+
+// console.log(myArr.includes("Hitesh"));
+// console.log(myArr.includes("Aks"));
+
+// account = 333333333
+// console.log(account);
+
+// accountName = "Aditya"
+// console.log(accountName);
+// console.log(typeof accountName,accountName);
+
+// const string3 = new String("Piya")
+// console.log(string3);
+
+
+
+// let myArr3 = [1,2,3,4,5]
+// let newArr = myArr3.join()
+// console.log(myArr3);
+// console.log(typeof myArr3);
+
+// console.log(newArr);
+// console.log(typeof newArr);
+
+ 
+
+let array = [1,2,3,4,5]
+console.log( " A" ,array);
+
+let array2 = array.slice(1,3)
+console.log(array2);
+console.log("B", array);
+
+
+let array3 = array.splice(1,3)
+console.log(array3);
+console.log("C",array);
+
+
+
