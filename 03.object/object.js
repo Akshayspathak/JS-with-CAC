834d094e78d6a186e2d6417e26ea9b0cce334a29
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2025-02-11 10:52:49 +0000

    object done

---
+++ 03.object/object.js
@@ -0,0 +1,37 @@
+const mySym = Symbol("key1")
+
+let Jsuser = {
+    Name: "Adi",
+    fullName: "Adi Mule",
+    age: 22,
+    [mySym]: "key1",
+    account:212121212,
+    accountPassword:"Adi@123",
+    isloggedIn: false,
+    lastDay: ["Monday", "Sunday"]
+}
+
+// console.log(Jsuser["fullName"]);
+// console.log(Jsuser[mySym]);
+// console.log(Jsuser["isloggedIn"]);
+
+
+
+Jsuser.isloggedIn = true
+
+// console.log(Jsuser["isloggedIn"]);
+// console.log(Jsuser);
+// console.log(Jsuser[mySym]);
+
+
+Jsuser.greeting = function(){
+    console.log("Hello JS User");
+    
+Jsuser.greeting2 = function(){
+    console.log(`Hello JS user, ${this.Name}`);
+    console.log(`Hello JS user, ${this.age}`);
+    
+}
+}
+console.log(Jsuser.greeting());
+console.log(Jsuser.greeting2());
