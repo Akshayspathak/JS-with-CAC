ae588c27b3283789b5df827ae986bceb93b8fc3f
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2025-02-14 07:35:32 +0000

    done

---
+++ 06.control_flow/one.js
@@ -0,0 +1,21 @@
+// if
+
+// let temp = 41
+// if ( temp === 40) {
+//     console.log(`less than 50`);
+// }else{
+
+
+
+// console.log(`temp greter  than 50`);
+// }
+
+
+   const score = 200
+   if (score < 100 ) {
+    let power = "fly"
+    console.log(`User power: ${power}`);
+   }else{
+    console.log(`user score is greter than 200`);
+    
+   }
