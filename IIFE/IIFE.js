a9ab1bbd562624334bea58a32ad8dc1a65e33690
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2025-02-14 07:13:03 +0000

    IIFE

---
+++ IIFE/IIFE.js
@@ -0,0 +1,106 @@
+// Immediately Invoked Functin Expression
+
+// 
+
+// function chai() {
+//     console.log(`DB CONNECTED`);
+        
+// }
+// chai();
+
+
+// IIFE
+
+
+// ( function chai2() {
+//     console.log(`DB Connected`);
+
+    
+// })();
+
+// // Arrow Function
+
+// ((name) => {
+//     console.log(`DB Connect Arrow Function, using ${name}`);
+    
+// })("Akshay")
+
+
+
+
+
+
+
+
+// *********************************************** IIFE ********************************************************
+
+
+// IIFE
+
+// (function chai() { 
+//     console.log(`DB Coonected3`);
+    
+// }) ();
+
+// Arrow Function
+
+// ((user)=>{console.log(user);
+// })('Akshay is connect')
+
+
+// ****************************************************************************************************************
+
+// const accountId =(user) => ({user:"Akshay"})
+// console.log(accountId());
+
+// const addTwoNum = (num1,num2) =>(num1 + num2)
+// console.log(addTwoNum(2,4));
+
+// const accountpass = (password) => ({password:"Akshay"})
+// console.log(accountpass());
+
+
+// let user = "Akshay Sudhir Pathak"               // String
+// console.log(user); 
+
+
+// const obj = {
+//     user:"akshay",
+//     email: "Hc@google.com",
+//     pass: "userName"
+// }
+
+// console.log(obj.user);
+// console.log(obj.email);
+// console.log(obj.pass);
+// console.log(typeof obj);
+
+
+// function user7() {
+//   console.log("Akku");
+
+// }
+// user7()
+
+
+// function sayMyName() {
+//     console.log("H");
+// console.log("I");
+// console.log("T");
+// console.log("E");
+// console.log("S");
+// console.log("H");
+// }
+// sayMyName()
+
+
+// const add = (num1,num2) =>(num1 * num2)
+// console.log(add(1,2));
+
+
+// ((name) =>{
+// console.log(`your name is ${name}`);
+
+// }) ("Mia")
+
+
