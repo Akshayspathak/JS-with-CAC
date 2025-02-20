58bdc421b991fe9b81b1ff3c175cf02d11333b34
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2025-02-20 08:19:09 +0000

    done

---
+++ Projects/01.color-change/src/App.jsx
@@ -0,0 +1,13 @@
+import Click from "./Components/Click"
+
+function App() {
+  
+
+  return (
+    <>
+    <Click></Click>
+    </>
+  )
+}
+
+export default App
---
+++ Projects/01.color-change/src/Components/Click.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+function Click() {
+const [buttonColor,setButtonColor] = useState("")
+const handleButtonClick = (color) => {
+  setButtonColor(color)
+}
+  return (
+    <div className="Click">
+
+      <button className=" p-5 m-5 rounded-lg bold " onClick={()=>handleButtonClick("red")}
+    style={{ backgroundColor: buttonColor === "red" ? "red" : "lightgray"}}> Red
+      </button>
+      <button className=" p-5 m-5 rounded-lg bold " onClick={()=>handleButtonClick("green")}
+    style={{ backgroundColor: buttonColor === "green" ? "green" : "lightgray"}}> Green
+      </button>
+      <button className=" p-5 m-5 rounded-lg bold " onClick={()=>handleButtonClick("yellow")}
+    style={{ backgroundColor: buttonColor === "yellow" ? "yellow" : "lightgray"}}> Yellow
+      </button>
+    </div>
+  )
+}
+export default Click
