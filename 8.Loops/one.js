c4dd39b5bcafb5b4c03574822fee25a8f6602658
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2025-02-21 11:45:54 +0000

    done

---
+++ 8.Loops/one.js
@@ -0,0 +1,53 @@
+
+
+for (let index = 0; index <= 10; index++) {
+    const element = index;
+    if (element == 5) {
+        // console.log("5 is best number");
+        
+    }
+    // console.log(element);
+}
+
+
+
+for (let i = 1; i <= 10; i++) {
+    // console.log(`out loop value: ${i}`);
+    
+    for (let j = 1; j <= 10; j++) {
+        // console.log(`inner loop value ${j} & inner loop ${i}`);
+        // console.log( i + "*" + j + ' = ' + i*j);
+        
+        
+    }
+    
+}
+
+Array = ["adi","piya","motyaa"]
+for (let index = 0; index < Array.length; index++) {
+    const element = Array[index];
+    // console.log(element);
+    
+    
+}
+
+
+for (let i = 0; i < 20; i++) {
+    if (i == 10 ) {
+        // console.log("Akshay");
+        break
+        
+        
+    }
+    // console.log(`Value of i is ${i}`);   
+}
+
+
+for (let i = 1; i < 50; i++) {
+    if (i == 4) {
+        console.log("Akshay");
+        continue
+    }
+    console.log(`Value of i is ${i}`);
+    
+}
