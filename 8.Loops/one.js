ac032c6f463d7adaa5a44c0aa32f2e45945baa78
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2025-02-21 12:36:56 +0000

    done

--- 8.Loops/one.js
+++ 8.Loops/one.js
@@ -1,4 +1,4 @@
-
+// for Loop
 
 for (let index = 0; index <= 10; index++) {
     const element = index;
---
+++ 8.Loops/two.js
@@ -0,0 +1,14 @@
+// while loop
+let index = 0
+while (index <=10) {
+    // console.log(`value of index is ${index}`);
+    index = index + 2
+    
+}
+
+let i = 0
+while (i<=10) {
+    console.log(`value of i is ${i}`);
+    i++
+    
+}
