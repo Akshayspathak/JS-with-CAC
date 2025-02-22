7a1dd4b30917f1419906157a7c820768e6ed8c00
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2025-02-22 06:57:01 +0000

    done

--- 8.Loops/two.js
+++ 8.Loops/two.js
@@ -1,14 +1,72 @@
 // while loop
-let index = 0
-while (index <=10) {
-    // console.log(`value of index is ${index}`);
-    index = index + 2
+// let index = 0
+// while (index <=10) {
+//     // console.log(`value of index is ${index}`);
+//     index = index + 2
     
+// }
+
+// let i = 0
+// while (i<=10) {
+//     console.log(`value of i is ${i}`);
+//     i++
+    
+// }
+
+// let index = 0
+// while (index <=20) {
+//     console.log(`value of index ${index}`);
+//     index = index + 3
+// }
+
+
+
+// let i = 0
+// while (i <= 5) {
+//     console.log(` value of i is ${i}`);
+//     i = i + 1
+// }
+
+// let myArr = ['Adi','piya','Motya']
+// let arr = 0
+// while (arr < myArr.length) {
+//     console.log(`value is ${myArr[arr]}`);
+//     arr = arr + 1
+    
+// }
+
+let array = ['java','javaScript','react']
+// let arr3 = 0
+// while (arr3 < array.length) {
+//     console.log(`value is ${array[arr3]}`);
+//     arr3 = arr3 + 1
+    
+// }
+
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);    
+// }
+
+
+// for (let i = 0; i <= 100; i++) {
+//  if (i % 2 == 1) {
+//     console.log(i);
+    
+//  }
+  
+    
+// }
+// odd Number
+for (let i = 0; i <= 100; i++) {
+   if (i % 2 == 1) {
+    console.log(i);
+   }
 }
 
-let i = 0
-while (i<=10) {
-    console.log(`value of i is ${i}`);
-    i++
-    
-}
+// Even Number
+for (let i = 0; i <= 100; i++) {
+    if (i % 2 == 0) {
+     console.log(i);
+    }
+ }
