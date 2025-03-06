9f2c6f3a9daf2281b8a4e8d0159a8850f414fbdb
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2025-03-06 11:37:56 +0000

    done

--- 8.Loops/each.js
+++ 8.Loops/each.js
@@ -1,10 +1,20 @@
 const coding = ['js','ruby','cpp','react']
-coding.forEach( (item, index, arr) => {
-    console.log(item, index, arr);
+// coding.forEach( (item, index, arr) => {
+//     console.log(item, index, arr);
     
-}   )
+// }   )
 
 // coding.forEach((val)=>{
 //     console.log(val);
     
 // })
+
+coding.forEach( (ak) => {
+    console.log(ak);
+    
+}    )
+
+coding.forEach( (item) => {
+    console.log(item);
+})
+
