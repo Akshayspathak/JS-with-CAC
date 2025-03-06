1fac908df8c6aad592706cc9bda8bf6fcc2dc4c6
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2025-03-06 09:29:34 +0000

    done

---
+++ 8.Loops/four.js
@@ -0,0 +1,77 @@
+// for if
+
+// const arr = [1,2,3,4,5]
+
+// for (const element of arr) {
+//     console.log(element);
+    
+// }
+
+// const greetings = 'hello world'
+// for (const greet of greetings) {
+//     console.log(`each char is ${greet}`);
+    
+// }
+
+
+// const value = "We are a frnd"
+// for (const element of value) {
+//     console.log(`${element}`);
+    
+// }
+
+
+
+// maps // Unique order
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// console.log(map);
+
+
+// for (const [key , value] of map) {
+//     console.log(key, ":-", value);
+    
+    
+// }
+
+
+// const map = new Map()
+// map.set('Name','Aaku')
+// map.set('Name2','piya')
+// map.set('Name3','vai')
+// console.log(map);
+
+
+// for (const [key,value] of map) {
+//     console.log(key,':-',value);
+    
+// }
+
+
+// const map = new Map()
+// map.set('name','adi')
+// map.set('age','21')
+// map.set('Bank','Account')
+// console.log(map);
+// for (const [key,value] of map) {
+//     console.log(key,':-', value);
+    
+// }
+
+
+
+const map = new Map()
+map.set('account','Id')
+map.set('accountName','Aaku')
+map.set('Bank','SBI')
+map.set('IFCECODE','SBI0075')
+console.log(map);
+
+for (const [key,value] of map) {
+    console.log(key, ":-", value);
+    
+}
+
--- 8.Loops/three.js
+++ 8.Loops/three.js
@@ -51,14 +51,14 @@ let mau = 0
 while (mau <= 10) 
     
     {
-        console.log(`this is value of ${mau}`);
+        // console.log(`this is value of ${mau}`);
         mau = mau + 2
 }
 
 let mau2 = 0
 let myArrayProgram = ['Adi',"Piya","ankit","vaibhav"]
 while (mau2 < myArrayProgram.length) {
-    console.log(` this is the ${myArrayProgram[mau2]}`);
+    // console.log(` this is the ${myArrayProgram[mau2]}`);
     mau2 = mau2 + 1
     
 }
@@ -69,3 +69,45 @@ while (mau2 < myArrayProgram.length) {
 //     console.log(`Value is ${myArray[setArray]}`);
 //     setArray = setArray + 1
 // }
+
+
+// do while
+
+// let score = 1
+// do {
+//     // console.log(`score is ${score}`);
+//     score = score + 1
+    
+    
+// } while (score <=10 );
+
+let marks = 9
+do {
+    console.log(`this is the values of ${marks}`);
+    marks  = marks + 1
+    
+} while (marks <= 10);
+
+let sumValue = 101
+
+do {
+    console.log(`this is value of ${sumValue}`);
+    sumValue = sumValue + 3
+    
+} while (sumValue <=200);
+
+let divValue = 101
+do {
+    console.log(`this is divValue of ${divValue}`);
+    divValue = divValue * 2
+    
+} while (divValue <= 4000);
+ 
+
+let max = 1000
+do {
+    console.log(`this is a max value of ${max}`);
+    max = max * 5
+    
+    
+} while ( max <= 300000 );
