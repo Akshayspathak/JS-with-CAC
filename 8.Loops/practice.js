ca4fd95899c896b021f552de564543098458cc25
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2025-03-07 10:29:30 +0000

    done

--- 8.Loops/practice.js
+++ 8.Loops/practice.js
@@ -49,5 +49,42 @@ const coding4 = myarr.map((num)=> num / 10)            // map
 // console.log(coding4);
 
 const chainValue = myarr.map((num)=> num+1).filter((num)=> num>=800)   // chaining
-console.log(chainValue);
+// console.log(chainValue);
+
+const chainingValue = myarr.filter((num)=> num >= 900).map((num)=> num *10)
+// console.log(chainingValue);
+
+
+// reduce
+
+const myred = [1,2,3,4]
+const myred2 = myred.reduce((acc,num)=> acc + num, 1)
+// console.log(myred2)
+
+
+const carsBrand = [
+    {
+        car:"Q3",
+        Brand:"Audi"
+    },
+    {
+        car:"A3",
+        Brand:"BMW"
+    },
+    {
+        car:"v1",
+        Brand:"Royal"
+    },
+    {
+        car:"oo7",
+        Brand:"Honda"
+    },
+    {
+        car:"x90",
+        Brand:"vok"
+    }
+]
+
+const carValue = carsBrand.reduce((acc,item)=> acc +item.car,0 )
+console.log(carValue);
 
