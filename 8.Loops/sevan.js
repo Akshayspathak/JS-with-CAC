541706ac3247bb64af3426a5cc04d9ae70ffe305
Akshay Pathak <93964429+Akshayspathak@users.noreply.github.com>
2025-03-07 07:45:59 +0000

    done

--- 8.Loops/sevan.js
+++ 8.Loops/sevan.js
@@ -1,6 +1,16 @@
 //  map
 
-const myNumers = [1,2,3,4,5,6,7,8,9,10]
+// const myNumers = [1,2,3,4,5,6,7,8,9,10]
 
-const newNum = myNumers.map((num) => num+10)
-console.log(newNum);
+// const newNum = myNumers.map((num) => num*10)
+// console.log(newNum);
+
+// const newNums = myNumers.filter((num)=> num >= 5)
+// console.log(newNums);
+
+
+// chain
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+const newNums = myNums.map((num)=> num * 10).map((num)=> num + 1).filter((num)=> num >=50).map((num)=>num+1)
+console.log(newNums);
