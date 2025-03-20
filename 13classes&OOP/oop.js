aa3b8e3af5a7fbd60b69c77eca6d454e01989f6c
akshay <akshay0130@yahoo.com>
2025-03-20 16:17:15 +0530

    AllDone

--- 13classes&OOP/oop.js
+++ 13classes&OOP/oop.js
@@ -9,11 +9,28 @@
 // console.log(userOne);
 // console.log(userTwo);
 
-function myAccount(account, accountholder, bank, address) {
+function Account(account, accountholder, bank, address) {
   this.account = account;
   this.accountholder = accountholder;
   this.bank = bank;
   this.address = address;
 }
-const user = new myAccount(1234567812345, "aman", "NearBank", "UK");
+const user = new Account(1234567812345, "aman", "NearBank", "UK");
 console.log(user);
+
+function myValue(userName, age, password) {
+  this.username = userName;
+  this.age = age;
+  this.password = password;
+}
+
+const value = new myValue("Aksim", 22, 212121212121);
+console.log(value);
+
+function isLoggedIn(userName, password) {
+  this.userName = userName;
+  this.password = password;
+}
+
+const loggedIn = new isLoggedIn("Simak", "Aksim");
+console.log(loggedIn);
