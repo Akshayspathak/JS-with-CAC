e89b2e205e84e19273267b88aff15bf9a4f85d51
akshay <akshay0130@yahoo.com>
2025-03-20 15:14:05 +0530

    all clear

--- 13classes&OOP/oop.js
+++ 13classes&OOP/oop.js
@@ -1,9 +1,19 @@
-function User(userName, loginCount, isLoggedIn) {
-  this.userName = userName;
-  this.loginCount = loginCount;
-  this.isLoggedIn = isLoggedIn;
+// function myUser(userName, loginCount, isLoggedIn) {
+//   this.userName = userName;
+//   this.loginCount = loginCount;
+//   this.isLoggedIn = isLoggedIn;
+
+// }
+// const userOne = new myUser("Joh", 12, true);
+// const userTwo = new myUser("Amar", 11, true);
+// console.log(userOne);
+// console.log(userTwo);
+
+function myAccount(account, accountholder, bank, address) {
+  this.account = account;
+  this.accountholder = accountholder;
+  this.bank = bank;
+  this.address = address;
 }
-const userOne = new User("Joh", 12, true);
-const userTwo = new User("Amar", 11, true);
-console.log(userOne);
-console.log(userTwo);
+const user = new myAccount(1234567812345, "aman", "NearBank", "UK");
+console.log(user);
