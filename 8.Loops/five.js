6d1bbab0a5280418073b69b12edc94a171a1723c
akshay <akshay0130@yahoo.com>
2025-03-20 14:27:31 +0530

    All Done

--- 8.Loops/five.js
+++ 8.Loops/five.js
@@ -7,71 +7,53 @@
 
 // for (const key in myObj) {
 //     console.log(`${key} for file name is for ${myObj[key]}`);
-    
-        
+
 //     }
 
-
-// ex 
+// ex
 
 const myObj = {
-    aku:"Akshay",
-    adi:"Aditya",
-    aaku:"Aakash",
-    vai: "Vaibhav",
-}
+  aku: "Akshay",
+  adi: "Aditya",
+  aaku: "Aakash",
+  vai: "Vaibhav",
+  akuu: "ak",
+};
 for (const key in myObj) {
-    // console.log(`${key} for a full name is for ${myObj[key]}`);
-    
-    }
+  // console.log(`${key} for a full name is for ${myObj[key]}`);
+}
 
+const obj = {
+  Audi: "Q3",
+  BMW: "x2",
+  Maruti: "SX4",
+  Honda: "City",
+};
+for (const key in obj) {
+  console.log(`${key} for the Best Car Name ${obj[key]} `);
+}
 
-    const obj = {
-        Audi:"Q3",
-        BMW:"x2",
-        Maruti:"SX4",
-        Honda:"City" 
-    }
-    for (const key in obj) {
-        console.log(`${key} for the Best Car Name ${obj[key]} `);
-        
-            
-        }
-    
+// using Array
 
-        // using Array
-
-        const programming = ['js','cs','cpp','py']
-        for (const key in programming) {
-            console.log(programming[key]);
-            
-                
-            }
-        
-
+const programming = ["js", "cs", "cpp", "py"];
+for (const key in programming) {
+  console.log(programming[key]);
+}
 
 // const obj4 = [1,2,3,4,5,6,7]
 // for (const key in obj4) {
-   
-//         console.log(key);
-        
-//     }
- 
 
+//         console.log(key);
+
+//     }
 
 const myObj2 = {
-    js:'JavaScript',
-    cpp:'c++',
-
-}
+  js: "JavaScript",
+  cpp: "c++",
+};
 for (const key in myObj2) {
-    console.log(`${key} fullName is ${myObj2[key]}`);
-    
+  console.log(`${key} fullName is ${myObj2[key]}`);
 }
 for (const element of myObj2) {
-    console.log(element);
-    
+  console.log(element);
 }
-
-
-
