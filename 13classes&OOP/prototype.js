a4e84871469da494e00e661da5df135b51cf1053
akshay <akshay0130@yahoo.com>
2025-03-21 16:53:22 +0530

    done

---
+++ 13classes&OOP/prototype.js
@@ -0,0 +1,59 @@
+// let myName = "Aksim     ";
+// let myChannel = "Brobots     ";
+// console.log(myName.trueLength);
+// console.log(myChannel.trueLength);
+
+// function car(name, model, color, weight, speed) {
+//   this.name = name;
+//   this.model = model;
+//   this.color = color;
+//   this.weight = weight;
+//   this.speed = speed;
+// }
+
+// let car1 = new car("Tata", 2024, "gray", "500 kg", 300);
+
+// car.prototype.madeIn = "India";
+
+// console.log(car1.speed);
+
+// let anotherUserName = "ChaiAurCode     ";
+// Object.prototype.trueLength = function () {
+//   console.log(`${this}`);
+//   console.log(`True length is ${this.trim().length}`);
+// };
+// anotherUserName.trueLength();
+// "hitesh".trueLength();
+// "iceTea".trueLength();
+// "Aksim".trueLength();
+
+// let user = "Aksim";
+// String.prototype.name = function () {
+//   console.log(`${this}`);
+//   console.log(`My alpha latter is  ${this.trim().length}`);
+// };
+// user.name();
+// "Mia".name();
+
+// String.prototype.myValue = function () {
+//   console.log(`${this}`);
+//   console.log(`This is the my Length is ${this.trim().length}`);
+// };
+
+// "Aaku".myValue();
+// "Maudduuu".myValue();
+
+Object.prototype.userName = function () {
+  console.log(`${this}`);
+  console.log(`this is my new user ${this.trim().length}`);
+};
+
+"shivji".userName();
+
+String.prototype.myArray = function () {
+  console.log(`${this}`);
+  console.log(`This is my new Array ${this.trim().length}`);
+};
+
+"akshay".myArray();
+"Pathak".myArray();
